349d65a973a2d154b77df96da62e9b47fac46298
thaddee tyl <thaddee.tyl@gmail.com>
2011-07-31 17:33:41 +0200

    renaming discussions yield this commit.

--- fs.js
+++ fs.js
@@ -1,9 +1,9 @@
-/* server.js: run this with Node.js in the publish/ folder to start your server.
+/* fs.js: file system primitives
  * Copyright (c) 2011 Jan Keromnes, Thaddee Tyl. All rights reserved.
  * The following code is covered by the GPLv2 license. */
 
 var Camp = require ('./lib/camp.js');
 
-Camp.handle (/\/root\/(.*)/, function (query, path) {
+Camp.handle (/\/(.*)/, function (query, path) {
   path[0] = '/pencil.html';
 });
