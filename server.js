5ed43ada8e86f5ac710b08913c30ffa795472b73
Thaddee Tyl <thaddee.tyl@gmail.com>
2011-12-22 18:45:28 +0100

    [FUZZY] Major overhaul of fuzzy matching.
    The ideas behind this are:
    
    - We get all leafs at once from the server with the new "fuzzy" hook that is
      attached to the "fs" ajax hook (within a certain level depth, that may be
      changed on the interface if we provide the relevant widgets).
    
    - Every character entered in the fuzzy search bar yields a score calculation of
      all leafs downloaded, against the search we did.
    
    - We give a score according to:
    
      * actual matches
      * successive matches
      * characters coming after alphanumerical characters.
    
      Discriminating against the depth of tree seemed like a good idea, but in
      practice it was found unsatisfactory.
    
    Signed-off-by: Thaddee Tyl <thaddee.tyl@gmail.com>

--- server.js
+++ server.js
@@ -48,6 +48,7 @@ camp.handle (new RegExp(ROOT_PREFIX + '/(.*)'), function (query, path) {
       console.error(err);
       data.error = err.message;
       camp.Server.emit ('fsplugged', data);
+      return;
     }
     if (arbor.isoftype(file, 'text/plain')) {
       path[0] = '/pencil.html';
@@ -86,6 +87,8 @@ arbor.getroot (function (err, fsroot) {
 // Ajax FS API.
 
 camp.add ('fs', function (query) {
+  // `query` must have an `op` field, which is a String.
+  // It must also have a `path` field, which is a String.
   console.log('actions are', camp.Server.Actions['fs']);
   var data = {};
   console.log('SERVER:FS: got query', query);
@@ -122,14 +125,25 @@ camp.add ('fs', function (query) {
           camp.Server.emit ('fs', data);
         });
       });
+      break;
     case 'touch':
       //create file
     case 'rm':
       //delete file
     case 'cp':
       //copy file
+    case 'fuzzy':
+      // `query` must, here, also have a field `depth` (an integer).
+      arbor.getfile (query['path'], function (err, file) {
+        if (err) { data.err = err; camp.Server.emit('fs', data); return; }
+        file.subfiles(function(err, subfiles) {
+          data.leafs = subfiles;
+          camp.Server.emit('fs', data);
+        }, query['depth']);
+      });
+      break;
     default:
-      return {};
+      return;
   }
 }, function fs(data) {
   console.log('SERVER:FS:FS');
