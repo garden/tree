1e89f92e2df4681ad174b52ccbdb6f96b7ef1e89
Thaddee Tyl <thaddee.tyl@gmail.com>
2016-08-05 11:34:02 +0200

    Script to move files and their metadata

---
+++ tools/meta/mv.js
@@ -0,0 +1,27 @@
+// Change the position of a file in the tree.
+
+var source = process.argv[2];
+var target = process.argv[3];
+if (!source || !target) {
+  console.log('Usage: node ./tools/meta/mv.js /foo/bar /baz/bar');
+  process.exit();
+}
+
+var driver = require('../../lib/driver.js');
+var exec = require('child_process').execFileSync;
+var path = require('path');
+
+var sourceFile = path.join(__dirname, '..', '..', 'web', source);
+var targetFile = path.join(__dirname, '..', '..', 'web', target);
+var sourceMetafile = driver.metafile(source);
+var targetMetafile = driver.metafile(target);
+
+console.log(sourceFile, targetFile);
+console.log(sourceMetafile, targetMetafile);
+
+exec('mv', [sourceFile, targetFile]);
+exec('mkdir', ['-p', path.dirname(targetMetafile)]);
+exec('mv', [sourceMetafile, targetMetafile]);
+if (exec('ls', [path.dirname(sourceMetafile)]) === '') {
+  exec('rmdir', [path.dirname(sourceMetafile)]);
+}
--- tools/meta/path.js
+++ tools/meta/path.js
@@ -1,11 +1,6 @@
 // Convert a virtual path (given as a parameter) to the metafile (on disk).
 
 var driver = require('../../lib/driver.js');
-var sha3 = require('sha3');
-var path = require('path');
-
-var projectDir = path.join(__dirname, '..', '..');
-var metaRoot = path.join(projectDir, 'meta');
 
 var virtualPath = process.argv[2];
 if (!virtualPath) {
