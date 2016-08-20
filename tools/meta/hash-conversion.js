f9d457c74c61117278857071145300f727e0e095
Thaddee Tyl <thaddee.tyl@gmail.com>
2016-08-21 00:00:32 +0200

    Hash conversion script

--- tools/meta/hash-conversion.js
+++ tools/meta/hash-conversion.js
@@ -1,7 +1,7 @@
 // Convert from the old metadata storage design (using .DS-Store for folder
 // metadata) to a new design (using hashed paths /ab/cdefg…).
 
-var sha3 = require('sha3');
+var driver = require('../../lib/driver.js');
 var path = require('path');
 var fs = require('fs');
 var exec = require('child_process').execFileSync;
@@ -12,36 +12,7 @@ var oldMeta = path.join(projectDir, 'meta');
 var newMeta = path.join(projectDir, 'meta-new');
 fs.mkdirSync(newMeta);
 
-// The functions normalize(), pathHash() and metafile() are from lib/driver.js.
-
-var metaRoot = newMeta;
-
-function normalize(vpath) {
-  // The following features several Windows hacks.
-  if (/^[^\/]/.test(vpath)) {
-    vpath = '/' + vpath;
-  }
-  var npath = path.normalize(vpath
-      .replace(/\\/g,'/').replace(/\/\//g,'/')).replace(/\\/g,'/');
-  return npath;
-}
-
-// Convert a normalized path to a base64url hash.
-function pathHash(npath) {
-  var h = sha3.SHA3Hash(256);
-  h.update('' + npath);
-  var d = h.digest();
-  var base64 = Buffer.from(d, 'binary').toString('base64');
-  return base64.slice(0, -1)  // Remove the = at the end.
-    .replace(/\+/g, '-').replace(/\//g, '_');
-}
-
-function metafile(vpath) {
-  var hash = pathHash(normalize(vpath));
-  var hashStart = hash.slice(0, 2);
-  var hashEnd = hash.slice(2);
-  return path.join(metaRoot, hashStart, hashEnd);
-}
+driver.setMetaRoot(newMeta);
 
 var files = ('' + exec('find', [oldMeta]))
   .slice(0, -1)  // There is an empty newline at the end.
@@ -64,7 +35,7 @@ function copyToNewLocation(apath) {
     var source = apath;
   }
   var vpath = virtualize(apath);
-  var target = metafile(vpath);
+  var target = driver.metafile(vpath);
 
   // Remove the newline at the end.
   var dirname = '' + exec('dirname', [target]).slice(0, -1);
